d4f0bbf223a127bb2633b2e9e62398918155800a
Muhammed Emre Durdu <muhammedemredurdu@gmail.com>
2020-07-31 01:53:10 +0300

    Initial AppBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import {AppBar, Tabs, Tab, Container, Menu, MenuItem} from '@material-ui/core';
+import './style/home.css';
+import axios from "axios";
+import {Category, Home} from "@material-ui/icons";
+import TextField from "@material-ui/core/TextField";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import Link from "@material-ui/core/Link";
+// import logo from '/images/logo.png';
+
+// Server host
+const host = 'http://localhost:8081';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // States
+    const [anchorEl, setAnchorEl] = useState(null);
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [input, setInput] = useState(null);
+    const [surveys, setSurveys] = useState([]);
+    const [surveyLoading, setSurveyLoading] = useState(false);
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    // Mounting functions
+    /**
+     * componentDidMount
+     */
+    useEffect(() => {
+        setIsLoading(true);
+        // Sunucudan kategorileri alıyoruz
+        axios.get(host + '/category/findAll')
+            .then(res => {
+                setCategories(res.data);
+                setIsLoading(false);
+            })
+            .catch(err => {
+                setError(err);
+                setIsLoading(false);
+            });
+
+        // Sunucudan trend surveyleri alıyoruz
+        setSurveyLoading(true);
+        axios.get(host + '/survey/trends')
+            .then(res => {
+                setSurveyLoading(false);
+                setSurveys(res.data);
+            })
+            .catch(err => {
+                setSurveyLoading(false);
+                setError(err);
+            })
+    }, []);
+
+    //Custom functions
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    };
+
+    const login = () => {
+        alert(`username: ${username}, password: ${password}`);
+    };
+
+    // Custom styles
+    const loginTextField = {
+        color: '#9FAFC1',
+        marginRight: '0.7rem',
+        fontSize: 'smaller'
+
+
+    };
+    return (
+        <div className="App">
+            <AppBar>
+                <Tabs className={"tabBar"}>7
+                    <img className={'logo'} src="/logo.png" alt="logo" href={'/'}/>
+                    <Tab label={'Anasayfa'}/>
+                    <Tab label={'Kategoriler'} onClick={handleClick} aria-controls={'categories_menu'}/>
+                    <Menu
+                        id={'categories_menu'}
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleClose}
+                    >
+                        {categories && categories.map(category => (
+                            <MenuItem onClick={handleClose}>{category.name}</MenuItem>
+                        ))}
+
+                    </Menu>
+                    <form action="" className={'loginForm'} onSubmit={login}>
+                        <TextField className={'loginText'} size={'small'} id={'username'} label={'Kullanıcı Adı'}
+                                   onChange={event => setUsername(event.target.value)} variant={'outlined'}
+                                   inputProps={loginTextField}/>
+                        <TextField className={'loginText'} size={'small'} id={'password'} label={'Şifre'}
+                                   onChange={event => setPassword(event.target.value)} variant={'outlined'}
+                                   inputProps={loginTextField}/>
+                        <Button className={'loginButton'} type={'submit'}>Giriş</Button>
+                        <Link className={'signUp'} href="#">
+                            Kayıt ol
+                        </Link>
+                    </form>
+
+                </Tabs>
+            </AppBar>
+            <Toolbar/>
+            <Container maxWidth={'md'}>
+                <form className={'searchForm'} action="" onSubmit={handleSubmit}>
+                    <TextField className={'searchBar'} size={'small'} placeholder={'Örn: lorem, @username, ...'}
+                               id="outlined-basic" label="Arama" variant="outlined" onChange={handleChange}/>
+                </form>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
