b7ef6fcd5e20184f1c1f52be82bb544f986b70ab
kursadsaka <ksaka@etu.edu.tr>
2020-08-03 02:13:16 +0300

    Login, logout

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import axios from "axios";
 import TextField from "@material-ui/core/TextField";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import Link from "@material-ui/core/Link";
+import { Link } from 'react-router-dom';
 import {AppFooter} from "./components/AppFooter";
 import {Home} from "./pages/Home";
 import history from './history';
@@ -20,6 +20,7 @@ const host = 'http://localhost:8081';
 function App() {
     // States
     const [anchorEl, setAnchorEl] = useState(null);
+    const [anchorElAvatarMenu, setAnchorElAvatarMenu] = useState(null);
     const [categories, setCategories] = useState([]);
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
@@ -76,8 +77,21 @@ function App() {
         setAnchorEl(event.currentTarget);
     };
 
+    const handleAvatarClick = (event) => {
+        setAnchorElAvatarMenu(event.currentTarget);
+    };
+
+    const handleProfileClick = (event) => {
+        //setAnchorElProfile(event.currentTarget);
+
+    };
+
     const handleClose = () => {
         setAnchorEl(null);
+    };
+
+    const handleAvatarMenuClose = () => {
+        setAnchorElAvatarMenu(null);
     };
 
     const handleSubmit = () => {
@@ -88,8 +102,35 @@ function App() {
         setInput(e.target.value);
     };
 
-    const login = () => {
-        alert(`username: ${username}, password: ${password}`);
+    const login = (e) => {
+        e.preventDefault();
+        axios.get(host + '/user/isUser', {
+            params: {
+                username: username,
+                password: password
+            }
+        })
+            .then(res => {
+
+                if (res.data==='') {
+                    alert('hatali giris');
+                }
+                else {
+                    alert(`hi ${res.data.username}`);
+                    setUser(res.data);
+                }
+
+            })
+            .catch(err => {
+                setError(err);
+                alert('ops');
+            });
+
+    };
+
+    const logout = () => {
+        setUser(null);
+        handleAvatarMenuClose();
     };
 
     // Custom styles
@@ -98,46 +139,73 @@ function App() {
         marginRight: '0.7rem',
         fontSize: 'smaller'
     };
+
     return (
         <div className="App">
-            <AppBar>
-                <Tabs className={"tabBar"}>7
-                    <img className={'logo'} src="/logo.png" alt="logo" href={'/'}/>
-                    <Tab label={'Anasayfa'} href={'/'}/>
-                    <Tab label={'Kategoriler'} onClick={handleClick} aria-controls={'categories_menu'}/>
-                    <Menu
-                        id={'categories_menu'}
-                        anchorEl={anchorEl}
-                        keepMounted
-                        open={Boolean(anchorEl)}
-                        onClose={handleClose}
-                    >
-                        {categories && categories.map(category => (
-                            <MenuItem onClick={handleClose}>{category.name}</MenuItem>
-                        ))}
-
-                    </Menu>
-                    <form action="" className={'loginForm'} onSubmit={login}>
-                        <TextField className={'loginText'} size={'small'} id={'username'} label={'Kullanıcı Adı'}
-                                   onChange={event => setUsername(event.target.value)} variant={'outlined'}
-                                   inputProps={loginTextField}/>
-                        <TextField className={'loginText'} size={'small'} id={'password'} label={'Şifre'}
-                                   onChange={event => setPassword(event.target.value)} variant={'outlined'}
-                                   inputProps={loginTextField}/>
-                        <Button className={'loginButton'} type={'submit'}>Giriş</Button>
-                        <Link className={'signUp'} href="/sign-up">
-                            Kayıt ol
-                        </Link>
-                    </form>
-
-                </Tabs>
-            </AppBar>
-            <Toolbar/>
             <Router history={history}>
+                <AppBar>
+                    <Tabs className={"tabBar"}>7
+                        <img className={'logo'} src="/logo.png" alt="logo" href={'/'}/>
+                        <Link className={'AnasayfaLink'} to="/">
+                            <Tab className={'Anasayfa'} label={'Anasayfa'}/>
+                        </Link>
+                        <Tab label={'Kategoriler'} onClick={handleClick} aria-controls={'categories_menu'}/>
+                        {user && <Tab label={'Popüler'} href={'/'}/>}
+                        {user && <Tab label={'Mesajlar'} href={'/'}/>}
+                        <Menu
+                            id={'categories_menu'}
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
+                        >
+                            {categories && categories.map(category => (
+                                <MenuItem onClick={handleClose}>{category.name}</MenuItem>
+                            ))}
+
+                        </Menu>
+                        {!user ? <form action="" className={'loginForm'} onSubmit={login}>
+                            <TextField className={'loginText'} size={'small'} id={'username'} label={'Kullanıcı Adı'}
+                                       onChange={event => setUsername(event.target.value)} variant={'outlined'}
+                                       inputProps={loginTextField}/>
+                            <TextField className={'loginText'} size={'small'} id={'password'} label={'Şifre'}
+                                       onChange={event => setPassword(event.target.value)} variant={'outlined'}
+                                       inputProps={loginTextField}/>
+                            <Button className={'loginButton'} type={'submit'}>Giriş</Button>
+                            <Link className={'signUp'} to="/sign-up">
+                                Kayıt ol
+                            </Link>
+                        </form> : <Button className={'profileButton'} type={'submit'} onClick={handleAvatarClick} aria-controls={'avatar_menu'}>
+                            <img className={'profileImage'} src = 'profile.jpg' />
+                        </Button>}
+
+                        {user && <Menu
+                            id={'avatar_menu'}
+                            anchorEl={anchorElAvatarMenu}
+                            keepMounted
+                            open={Boolean(anchorElAvatarMenu)}
+                            onClose={handleAvatarMenuClose}
+                        >
+                            <Link className={'profileLink'} to={"/user/"+user.id}>
+                                <MenuItem className={'profile'}>
+                                    Profil
+                                </MenuItem>
+                            </Link>
+                            <MenuItem onClick={handleAvatarMenuClose}>Ayarlar</MenuItem>
+                            <Link className={'logoutLink'} onClick={logout} to={"/"}>
+                                <MenuItem className={'logout'}>
+                                    Çıkış
+                                </MenuItem>
+                            </Link>
+                        </Menu>}
+
+                    </Tabs>
+                </AppBar>
+                <Toolbar/>
                 <Route exact path={'/'}
                        component={() => <Home surveys={surveys} getSurveys={getSurveys} handleSubmit={handleSubmit}
                                               surveyLoading={surveyLoading} handleChange={handleChange}/>}/>
-                <Route path={'/sign-up'} component={Signup}/>
+                {!user && <Route path={'/sign-up'} component={Signup}/>}
                 {/*User login olduysa gidebileceği profil sayfası*/}
                 {user && <Route path={'/user/:id'} component={User}/>}
             </Router>
